55e42e39fe445dd1c64508cf7429fe2aaf15e9b3
Akiva <git@misanthropicgeek.com>
2014-12-04 20:05:31 -0600

    Adds jade templating.

--- src/compiler.js
+++ src/compiler.js
@@ -1,14 +1,15 @@
 (function () {
 	'use strict';
 
-	var md = require ('markdown').markdown;
+	var jade = require ('jade');
 	var io = require ('./io');
+	var md = require ('markdown').markdown;
 
 	var parentDir = 'resources/public/';
 	var postsPath = parentDir + 'posts/';
 	
 	function parsePage (filename) {
-		var body, frontMatter, matches, outputName, outputPath, pattern, srcFile;
+		var body, compiler, content, frontMatter, matches, outfile, output, path, pattern, srcFile;
 		
 		if (!filename) {
 			throw Error ("parsePage requires a filename.");
@@ -16,7 +17,6 @@
 
 		pattern = /(\{(?:.|\n)+\})(?:\n)+((.|\n)+)/;
 		srcFile = io.readFile (filename);
-
 		matches = srcFile.match (pattern);
 
 		if (matches.length === 0) {
@@ -26,9 +26,20 @@
 		frontMatter = JSON.parse (matches [1]);
 		body = md.toHTML (matches [2]);
 
-		outputName = io.createPostFilename (frontMatter.title, frontMatter.date);
-		outputPath = io.createPostDirectoryPath (frontMatter.date, postsPath);
+		content = {};
 
-		io.savePage (outputPath, outputName, body);
+		for (var attr in frontMatter) {
+			content [attr] = frontMatter [attr];
+		}
+
+		content.body = body;
+
+		outfile = io.createPostFilename (frontMatter.title, frontMatter.date);
+		path = io.createPostDirectoryPath (frontMatter.date, postsPath);
+		compiler = jade.compileFile ('resources/templates/post.jade', { pretty: true });
+
+		output = compiler (content);
+		
+		io.savePage (path, outfile, output);
 	}
 } ());
