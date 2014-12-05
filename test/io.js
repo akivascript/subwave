cc9993924725180998f924be56566b7da6bfd4bc
Akiva <git@misanthropicgeek.com>
2014-12-04 18:33:01 -0600

    Corrects an overly aggressive gitignore file.

---
+++ test/io.js
@@ -0,0 +1,49 @@
+(function () {
+	'use strict';
+
+	var expect = require ('chai').expect;
+	var io = require ('../src/io.js');
+
+	var testFilePath = 'test/files/';
+
+	describe ('IO', function () {
+		describe ('createPostFilename', function () {
+			it ('should produce the right kind of filename', function () {
+				var actual, expected, title, date;
+
+				title = "This Is the Title of a Post";
+				date = "2014-08-20 19:54";
+
+				expected = "2014-08-20-this-is-the-title-of-a-post.md";
+				actual = io.createPostFilename (title, date);
+
+				expect (actual).to.equal (expected);
+			});
+		});
+
+		describe ('createPostDirectoryPath', function () {
+			it ('should produce the correct directory path without parent path', function () {
+				var actual, date, expected;
+
+				date = "2014-08-20 19:54";
+
+				expected = "2014/08";
+				actual = io.createPostDirectoryPath (date);
+
+				expect (actual).to.equal (expected);
+			});
+
+			it ('should produce the correct directory path with parent path', function () {
+				var actual, date, expected, path;
+
+				date = "2014-08-20 19:54";
+				path = "public/posts/";
+
+				expected = "public/posts/2014/08";
+				actual = io.createPostDirectoryPath (date, path);
+
+				expect (actual).to.equal (expected);
+			});
+		});
+	});
+} ());
