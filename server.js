57d2052a044ca63fa55daad5631ab30b7da94a77
Akiva <git@misanthropicgeek.com>
2015-01-01 18:02:13 -0600

    Adds a port argument to server and makes it executable.

--- server.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	'use strict';
-
-	var connect = require ('connect');
-	var serve = require ('serve-static');
-
-	connect ().use (serve (__dirname + '/public/')).listen (8000);
-} ());
